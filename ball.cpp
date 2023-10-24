dcab58b52e8182f4c55602c3dcc850a49072058d
Lili Jiang <lily9393@gmail.com>
2023-10-24 00:56:46 -0500

    ball can take on any color

--- ball.cpp
+++ ball.cpp
@@ -1,17 +1,31 @@
 #include "Ball.h"
 
 #include <QBrush>
-#include <QLinearGradient>
+
 #include <QPen>
 
 Ball::Ball()
 {
-    QRadialGradient gradient(25, 25, 30, 15, 15);
-
-    gradient.setColorAt(0.0, QColor(255, 240, 240));
-    gradient.setColorAt(0.3, QColor(255, 50, 50));
-    gradient.setColorAt(0.7, QColor(100, 0, 0));
-
+    QRadialGradient gradient = generateGradient(Qt::blue);
     setBrush(QBrush(gradient));
     setPen(Qt::NoPen);
+}
+
+QRadialGradient Ball::generateGradient(const QColor &baseColor) const
+{
+    int r = baseColor.red();
+    int g = baseColor.green();
+    int b = baseColor.blue();
+
+    // Compute the bright, intermediate, and dark shades
+    QColor brightShade(qMin(r + 240, 255), qMin(g + 240, 255), qMin(b + 240, 255));
+    QColor intermediateShade(qMin(r + 50, 255), qMin(g + 50, 255), qMin(b + 50, 255));
+    QColor darkShade(qMax(r - 155, 0), qMax(g - 155, 0), qMax(b - 155, 0));
+
+    QRadialGradient gradient(25, 25, 30, 15, 15);
+    gradient.setColorAt(0.0, brightShade);
+    gradient.setColorAt(0.3, intermediateShade);
+    gradient.setColorAt(0.7, darkShade);
+
+    return gradient;
 }
--- ball.h
+++ ball.h
@@ -2,11 +2,16 @@
 #define BALL_H
 
 #include <QGraphicsEllipseItem>
+#include <QLinearGradient>
 
 class Ball : public QGraphicsEllipseItem
 {
 public:
     Ball();
+
+private:
+    // Generates the gradient to create a 3D-looking ball.
+    QRadialGradient generateGradient(const QColor &baseColor) const;
 };
 
 #endif // BALL_H

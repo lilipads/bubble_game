1d1c6ff4ce398b4090c77d76bb52f65b7355dc18
Lili Jiang <lily9393@gmail.com>
2023-10-24 00:50:59 -0500

    make ball 3d looking

--- ball.cpp
+++ ball.cpp
@@ -6,10 +6,12 @@
 
 Ball::Ball()
 {
-    QLinearGradient gradient(0, 0, 50, 50);
-    gradient.setColorAt(0, Qt::red);
-    gradient.setColorAt(1, Qt::gray);
+    QRadialGradient gradient(25, 25, 30, 15, 15);
+
+    gradient.setColorAt(0.0, QColor(255, 240, 240));
+    gradient.setColorAt(0.3, QColor(255, 50, 50));
+    gradient.setColorAt(0.7, QColor(100, 0, 0));
 
     setBrush(QBrush(gradient));
-    //    setPen(QPen(Qt::black, 2));
+    setPen(Qt::NoPen);
 }

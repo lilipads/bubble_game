f571093f6e886c8e4b7cb06bf3475ab038f9c4d4
Lili Jiang <lily9393@gmail.com>
2023-10-24 23:46:17 -0500

    add skeleton for Game

--- game.cpp
+++ game.cpp
@@ -9,3 +9,31 @@ QGraphicsScene *Game::gameScene() const
 {
     return m_board->scene();
 }
+
+void Game::scoreAndUpdateBoard(const Coordinate coordinate)
+{
+    const int delta_score = getDeltaScoreAndEliminateLines(coordinate);
+    m_score += delta_score;
+    if (delta_score == 0) {
+        addNewBalls();
+    }
+}
+
+void Game::addNewBalls()
+{
+    // TODO: add balls
+    displayNextBatchColors();
+}
+
+int Game::getDeltaScoreAndEliminateLines(const Coordinate coordinate)
+{
+    const int delta_score = 0;
+    // Horizontal.
+    // Vertical.
+    // Forward slash.
+    // Backward slach.
+    if (delta_score > 0) {
+        // Removes the origin.
+    }
+    return delta_score;
+}
--- game.h
+++ game.h
@@ -11,10 +11,23 @@ public:
     explicit Game(QWidget *parent = nullptr);
     QGraphicsScene *gameScene() const;
 
+    // Updates m_score and eliminates line if any line is formed at the `coordinate`.
+    void scoreAndUpdateBoard(const Coordinate coordinate);
+    int getScore() { return m_score; }
+
 private:
     const int kGridSize = 9;
-
+    const int kNewBalls = 3;
     Board *m_board;
+    int m_score;
+
+    void displayNextBatchColors();
+    void addNewBalls();
+    int getDeltaScoreAndEliminateLines(const Coordinate coordinate);
+
+    //  Returns pointers to consecutive consecutive tiles with the same color ball as the ball at `origin` if exists in a given `direction`. This does not include the Tile at the `origin`.
+    std::vector<Tile *> getConsecutiveTilesWithSameColorBalls(const Coordinate origin,
+                                                              const Coordinate direction);
 };
 
 #endif // GAME_H
